cfba3ea1f3d2bc3057fb8863783dbfd7dc499fe9
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-10-25 17:23:56 +0700

    fix(codelab): kegiatan 2 on modul 3

--- modul3/latihan/kegiatan2.py
+++ modul3/latihan/kegiatan2.py
@@ -5,6 +5,7 @@ data = ["3 minggu 3 hari 7 jam 21 menit",
         "5 minggu 5 hari 8 jam 11 menit",
         "7 minggu 1 hari 5 jam 33 menit"]
 
+# Fungsi curried_converter untuk mengonversi nilai integer
 def curried_converter(weeks):
     def inner_curried(days):
         def inner_inner_curried(hours):
@@ -14,13 +15,18 @@ def curried_converter(weeks):
         return inner_inner_curried
     return inner_curried
 
+# Fungsi untuk memfilter hanya nilai integer dari daftar
+def filter_integer_values(item):
+    return list(filter(lambda x: x.isdigit(), item.split()))
+
 int_values = []
+
 for item in data:
-    parts = item.split()
-    weeks = int(parts[0])
-    days = int(parts[2])
-    hours = int(parts[4])
-    minutes = int(parts[6])
+    integer_values = list(map(int, filter_integer_values(item)))
+    weeks = integer_values[0]
+    days = integer_values[1]
+    hours = integer_values[2]
+    minutes = integer_values[3]
 
     converted_value = curried_converter(weeks)(days)(hours)(minutes)
     int_values.append(converted_value)
