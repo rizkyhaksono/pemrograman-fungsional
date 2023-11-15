c8e25654f7ae836c45ed9a4e0fcbef2895f18ef3
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-11-16 02:17:23 +0700

    feat(latihan): fix on kegiatan 4-6

--- modul4/latihan/kegiatan4.py
+++ modul4/latihan/kegiatan4.py
@@ -1,36 +1,43 @@
-# Percobaan 4
 import math
 
-# Fungsi untuk translasi
-def translasi(x, y, tx, ty):
-    new_x = x + tx
-    new_y = y + ty
-    return new_x, new_y
+def translasi(x, y):
+    def inner(tx, ty):
+        new_x = x + tx
+        new_y = y + ty
+        return new_x, new_y
+    return inner
 
-# Fungsi untuk dilatasi
-def dilatasi(x, y, sx, sy):
-    new_x = x * sx
-    new_y = y * sy
-    return new_x, new_y
+def dilatasi(x, y):
+    def inner(sx, sy):
+        new_x = x * sx
+        new_y = y * sy
+        return new_x, new_y
+    return inner
+  
+def float_output(function):
+    def inner(*args, **kwargs):
+        result = function(*args, **kwargs)
+        return f"{result[0]:.5f}, {result[1]:.5f}"
+    return inner
 
-# Fungsi untuk rotasi
+@float_output
 def rotasi(x, y, sudut):
     radian = math.radians(sudut)
     new_x = x * math.cos(radian) - y * math.sin(radian)
     new_y = x * math.sin(radian) + y * math.cos(radian)
     return new_x, new_y
-
+  
 # Titik awal
 titik_P = (3, 5)
 
 # Translasi
-titik_setelah_translasi = translasi(titik_P[0], titik_P[1], 2, -1)
+titik_setelah_translasi = translasi(*titik_P)(2, -1)
 print("Koordinat setelah translasi:", titik_setelah_translasi)
 
 # Dilatasi
-titik_setelah_dilatasi = dilatasi(titik_P[0], titik_P[1], 2, -1)
+titik_setelah_dilatasi = dilatasi(*titik_P)(2, -1)
 print("Koordinat setelah dilatasi:", titik_setelah_dilatasi)
 
 # Rotasi
-titik_setelah_rotasi = rotasi(titik_P[0], titik_P[1], 30)
+titik_setelah_rotasi = rotasi(*titik_P, 30)
 print("Koordinat setelah rotasi:", titik_setelah_rotasi)
--- modul4/latihan/kegiatan6.py
+++ modul4/latihan/kegiatan6.py
@@ -3,10 +3,9 @@ def point(x, y):
     return x, y
 
 def line_equation_of(x1, y1, M):
-    #TODO 1: tulis rumus untuk mendapatkan nilai C disini
     C = y1 - M * x1
     return f"y = {M:.2f}x + {C:.2f}"
 
 # Ubah nilai input dengan 3 digit NIM akhir 189
-print("Persamaan garis yang melalui titik (1,8) dan bergradien 9:")
-print(line_equation_of(1, 8, 9))
+print("Persamaan garis yang melalui titik (3,2) dan bergradien 5:")
+print(line_equation_of(3, 2, 5))
