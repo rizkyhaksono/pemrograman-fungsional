2db2f2ac8166bc5cc2a74e4271d1a09fcfc2beda
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-11-29 19:24:42 +0700

    feat(latihan): change py to ipynb

--- modul5/latihan/percobaaa1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-# modifikasi
-xpoints = np.array([1, 8])
-ypoints = np.array([3, 10])
-
-plt.plot(xpoints, ypoints)
-plt.show()
--- modul5/latihan/percobaan4.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x = [1, 2, 3, 4, 5]
-y = [3, 7, 2, 8, 5]
-
-# plt.scatter(x, y)
-plt.scatter(x, y, label="sX", marker="*", color="hotpink")
-
-plt.show()
